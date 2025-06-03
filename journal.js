f41a5986ee24fd408e868574535799fcecad7805
Olive Njoroge <njorogeolive001@gmail.com>
2025-06-03 10:28:39 +0300

    Populated the mongoose model

--- journal.js
+++ journal.js
@@ -1,1 +1,11 @@
+//Created a mongoose model
 const mongoose = require('mongoose');
+
+const journalSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    content: {type: String, require: true},
+    date: {type: Date, default: Date.now}
+});
+
+//export the model
+module.exports = mongoose.model("Journal", journalSchema);
