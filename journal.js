16e8b6b315d9eabcff44ba2d83bed9c6a64d8f6b
Olive Njoroge <njorogeolive001@gmail.com>
2025-06-04 07:16:54 +0300

    Corrected a typo

--- journal.js
+++ journal.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const journalSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    content: {type: String, require: true},
+    content: {type: String, required: true},
     date: {type: Date, default: Date.now}
 });
 
